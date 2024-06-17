72e38a1a1c910d109beed48b6aacd9ec75822005
Deraclassic <chiderauzoigwe2@gmail.com>
2024-06-17 15:55:05 +0100

    spring security config

--- src/main/java/com/Java020/MonieFlex/infrastructure/controller/AuthenticationController.java
+++ src/main/java/com/Java020/MonieFlex/infrastructure/controller/AuthenticationController.java
@@ -1,4 +1,12 @@
 package com.Java020.MonieFlex.infrastructure.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+
 public class AuthenticationController {
 }
